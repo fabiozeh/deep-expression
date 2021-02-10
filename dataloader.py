Added validation dataloader for computing loss per piece and helping with testing. midi generation rewritten with aux function. Fixed a bug in timingDevLocal calculation.

dataloader.py
@@ -1,11 +1,23 @@
 import torch
+import numpy as np
 
 
-class DataGenerator(torch.utils.data.Dataset):
-    def __init__(self, data, vocab_col, sequence_length, output_cols=None):
+def pitchVocabularyFmt(X, vocab_col):
+    """
+    Produces the tensors for training with a pitch vocabulary encoding.
+    """
+    pitch = torch.LongTensor(X[:, vocab_col])
+    score_feats = torch.cat([torch.FloatTensor(X[:, :vocab_col]),
+                            torch.FloatTensor(X[:, vocab_col + 1:])], dim=1)
+    return pitch, score_feats
+
+
+class TrainDataset(torch.utils.data.Dataset):
+    def __init__(self, data, vocab_col, sequence_length, output_cols=None, dummy=False):
         self.data = data
         self.sequence_length = sequence_length
         self.vocab_col = vocab_col
+        self.dummy = dummy
         self.indexes = []
         if output_cols is None:
             self.output_cols = data[0][0][1].columns
@@ -23,29 +35,28 @@ class DataGenerator(torch.utils.data.Dataset):
         # np.random.shuffle(self.indexes)  # always shuffle once
 
     def __len__(self):
-        return len(self.indexes)
+        if self.dummy:
+            return 32
+        else:
+            return len(self.indexes)
 
     def __getitem__(self, index):
-        (seq, stride) = self.indexes[index]
+        (seq, offset) = self.indexes[index]
         (X, Y, _) = self.data[seq][0]
         Y = Y.loc[:, self.output_cols]
-        if stride + self.sequence_length <= X.shape[0]:
-            X = X.iloc[stride:stride + self.sequence_length, :].to_numpy(dtype='float64')
-            Y = Y.iloc[stride:stride + self.sequence_length, :].to_numpy(dtype='float64')
-            length = self.sequence_length
+        if offset + self.sequence_length <= X.shape[0]:
+            X = X.iloc[offset:offset + self.sequence_length, :].to_numpy(dtype='float64')
+            Y = Y.iloc[offset:offset + self.sequence_length, :].to_numpy(dtype='float64')
         else:
-            length = X.shape[0] - stride
-            X = X.iloc[stride:, :].to_numpy(dtype='float64')
-            Y = Y.iloc[stride:, :].to_numpy(dtype='float64')
+            X = X.iloc[offset:, :].to_numpy(dtype='float64')
+            Y = Y.iloc[offset:, :].to_numpy(dtype='float64')
 
-        pitch = torch.LongTensor(X[:, self.vocab_col])
-        harmRhythm = torch.cat([torch.FloatTensor(X[:, :self.vocab_col]),
-                                torch.FloatTensor(X[:, self.vocab_col + 1:])], dim=1)
-        return pitch, harmRhythm, torch.FloatTensor(Y), length
+        pitch, score_feats = pitchVocabularyFmt(X, self.vocab_col)
+        return pitch, score_feats, torch.FloatTensor(Y), X.shape[0]
 
     @staticmethod
     def collate_fn(batch):
-        pitch, harmRhythm, Y, length = (
+        pitch, score_feats, Y, length = (
             [element[0] for element in batch],
             [element[1] for element in batch],
             [element[2] for element in batch],
@@ -54,7 +65,77 @@ class DataGenerator(torch.utils.data.Dataset):
 
         # Pad batch
         pitch = torch.nn.utils.rnn.pad_sequence(pitch, batch_first=False)
-        harmRhythm = torch.nn.utils.rnn.pad_sequence(harmRhythm, batch_first=False)
+        score_feats = torch.nn.utils.rnn.pad_sequence(score_feats, batch_first=False)
         Y = torch.nn.utils.rnn.pad_sequence(Y, batch_first=False)
 
-        return pitch, harmRhythm, Y, torch.LongTensor(length).view(-1)
+        return pitch, score_feats, Y, torch.LongTensor(length).view(-1)
+
+
+class ValidationDataset(torch.utils.data.Dataset):
+    """
+    This dataset splits pieces according to sequence length to form one batch
+    per piece and sliding the input window according to stride.
+    """
+    def __init__(self, data, vocab_col, sequence_length, output_cols=None, stride=0, device=None):
+        self.data = data
+        self.sequence_length = sequence_length
+        self.vocab_col = vocab_col
+        self.indexes = []
+        if stride == 0:
+            self.stride = self.sequence_length
+        else:
+            self.stride = stride
+        self.device = device
+
+        assert self.stride <= self.sequence_length, "Invalid combination of stride and sequence_length"
+
+        if output_cols is None:
+            self.output_cols = data[0][0][1].columns
+        else:
+            self.output_cols = output_cols
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        (X, Y, _) = self.data[index][0]
+        Y = Y.loc[:, self.output_cols]
+        if X.shape[0] % self.stride == 0:
+            batch_size = int(X.shape[0] / self.stride)
+        else:
+            batch_size = int(X.shape[0] / self.stride + 1)
+
+        pitch_batch = np.zeros((self.sequence_length, batch_size))
+        score_feats_batch = np.zeros((self.sequence_length, batch_size, X.shape[1] - 1))
+        Y_batch = np.zeros((self.sequence_length, batch_size, len(self.output_cols)))
+        length_batch = np.zeros(batch_size)
+
+        xind = 0
+        for i in range(batch_size):
+            if xind + self.sequence_length <= X.shape[0]:
+                pitch_batch[:, i] = X.iloc[xind:xind + self.sequence_length, self.vocab_col].to_numpy(dtype='int64')
+                score_feats_batch[:, i, :self.vocab_col] = X.iloc[xind:xind + self.sequence_length, :self.vocab_col].to_numpy(dtype='float64')
+                score_feats_batch[:, i, self.vocab_col:] = X.iloc[xind:xind + self.sequence_length, self.vocab_col + 1:].to_numpy(dtype='float64')
+                Y_batch[:, i, :] = Y.iloc[xind:xind + self.sequence_length, :].to_numpy(dtype='float64')
+                length_batch[i] = self.sequence_length
+            else:
+                sz_last = X.shape[0] - xind
+                pitch_batch[:sz_last, i] = X.iloc[xind:, self.vocab_col].to_numpy(dtype='int64')
+                score_feats_batch[:sz_last, i, :self.vocab_col] = X.iloc[xind:, :self.vocab_col].to_numpy(dtype='float64')
+                score_feats_batch[:sz_last, i, self.vocab_col:] = X.iloc[xind:, self.vocab_col + 1:].to_numpy(dtype='float64')
+                Y_batch[:sz_last, i, :] = Y.iloc[xind:, :].to_numpy(dtype='float64')
+                length_batch[i] = sz_last
+            xind += self.stride
+
+        if self.device is not None:
+            pitch_batch = torch.LongTensor(pitch_batch, device=self.device)
+            score_feats_batch = torch.FloatTensor(score_feats_batch, device=self.device)
+            Y_batch = torch.FloatTensor(Y_batch, device=self.device)
+            length_batch = torch.LongTensor(length_batch, device=self.device)
+        else:
+            pitch_batch = torch.LongTensor(pitch_batch)
+            score_feats_batch = torch.FloatTensor(score_feats_batch)
+            Y_batch = torch.FloatTensor(Y_batch)
+            length_batch = torch.LongTensor(length_batch)
+
+        return pitch_batch, score_feats_batch, Y_batch, length_batch

expression_modeling.py
@@ -3,6 +3,8 @@ import math
 
 import numpy as np
 import pandas as pd
+import pretty_midi
+
 
 #
 # Classes
@@ -137,12 +139,11 @@ def buildPart(notearray, levels, srate):
         if local_tempo_notes:
             local_tempo_notes = [x for x in local_tempo_notes if x[0] >= local_tempo_notes[-1][0] - 4]  # keep 4 beats before previous note
 
+        # if there's more than 1 beat logged, update local_tempo
         if local_tempo_notes and (local_tempo_notes[-1][0] - local_tempo_notes[0][0]) > 1 and (local_tempo_notes[-1][1] - local_tempo_notes[0][1]) > 0:
             local_tempo = (local_tempo_notes[-1][0] - local_tempo_notes[0][0]) / (local_tempo_notes[-1][1] - local_tempo_notes[0][1])
-            current_grid_time_local += (beat - local_tempo_notes[-1][0]) / local_tempo
-        else:
-            # keep the same local_tempo from previous entry.
-            current_grid_time_local = entry['start_time'] / srate
+
+        current_grid_time_local += (beat - current_beat) / local_tempo
 
         current_beat = beat
         dur = entry['end_beat']
@@ -304,21 +305,6 @@ def dissonancePresent(pitches, key, mode):
     pitches = [(p - scale + 12) % 12 for p in pitches]
     return not np.all(np.isin(pitches, lookup))
 
-#
-# Timing calculations (unused)
-#
-
-
-def localTempo(notes):
-    if not notes:
-        return np.nan
-    notes.sort(key=(lambda x: x.startTime))
-    totalBeats = notes[-1].startBeat - notes[0].startBeat
-    totalS = notes[-1].startTime - notes[0].startTime
-    if totalBeats <= 1 or totalS <= 0:
-        return np.nan
-    else:
-        return totalBeats / totalS
 
 #
 # Phrase recognition
@@ -433,7 +419,7 @@ def buildNoteLevelDataframe(piece, transpose=0):
         "isDissonance": [],     # True if pitch not in piece.mode scale
         "startTime": [],        # onset time in s
         "durationSecs": [],     # note duration in s
-        "ioiRatio": [],         # ioi in beats / ioi in s (local tempo estimate)
+        "ioiRatio": [],         # ioi in s / ioi in beats (local tempo estimate)
         "timingDev": [],        # total onset deviation time assuming steady tempo
         "timingDevLocal": [],   # onset deviation time from local tempo
         "localTempo": [],       # moving average of tempo before onset
@@ -493,3 +479,70 @@ def buildNoteLevelDataframe(piece, transpose=0):
     #     df.drop([attrib], axis=1, inplace=True)
 
     return df
+
+
+def midi_performance(test, prediction, moments, ix_to_lex, method='ioiRatio'):
+    """
+    Returns a pretty_midi object with a performance generated according to the
+    given numpy array of performance actions. Method specifies which measurement
+    of tempo and timing deviations was used.
+    """
+    tempo = math.exp(test[1].localTempo.iloc[0] * test[2][0, 1] + test[2][0, 0])
+    pm = pretty_midi.PrettyMIDI(initial_tempo=tempo)
+    piano = pretty_midi.Instrument(1, is_drum=False, name='piano')
+    violin = pretty_midi.Instrument(41, is_drum=False, name='violin')
+    pm.instruments.append(piano)
+    pm.instruments.append(violin)
+
+    if method == 'ioiRatio':
+        # for now, ratio calculated wrt next note of same instrument, which is
+        # inconvenient and may produce unsynced performances. Also, durations
+        # are copied from reference performance (since the model wasn't trained
+        # on articulation).
+        ioiRatio = prediction * test[2][2, 1] + test[2][2, 0]
+
+        piano_ioi = (0, 1)
+        violin_ioi = (0, 1)
+        for x, y, dev in zip(test[0].itertuples(), test[1].itertuples(), ioiRatio):
+            pitch = ix_to_lex.get(x.pitch)
+            if pitch:
+                pitch = pitch[0]
+                if x.instrument_1:
+                    if piano.notes:
+                        start = piano.notes[-1].start + piano_ioi[0] * piano_ioi[1]
+                        end = start + (y.durationSecs * moments['durationSecs'][1] + moments['durationSecs'][0])
+                    else:
+                        start = y.startTime * moments['startTime'][1] + moments['startTime'][0]
+                        end = start + (y.durationSecs * moments['durationSecs'][1] + moments['durationSecs'][0])
+                    piano.notes.append(pretty_midi.Note(100, pitch, start, end))
+                    piano_ioi = (x.ioi * moments['ioi'][1] + moments['ioi'][0] + 1e-6, dev)
+                else:
+                    if violin.notes:
+                        start = violin.notes[-1].start + violin_ioi[0] * violin_ioi[1]
+                        end = start + (y.durationSecs * moments['durationSecs'][1] + moments['durationSecs'][0])
+                    else:
+                        start = y.startTime * moments['startTime'][1] + moments['startTime'][0]
+                        end = start + (y.durationSecs * moments['durationSecs'][1] + moments['durationSecs'][0])
+                    violin.notes.append(pretty_midi.Note(100, pitch, start, end))
+                    violin_ioi = (x.ioi * moments['ioi'][1] + moments['ioi'][0] + 1e-6, dev)
+
+    elif method == 'timingDevLocal':
+        timingDevLocal = prediction[:, 0] * moments['timingDevLocal'][1] + moments['timingDevLocal'][0]
+        localTempo = prediction[:, 1] * test[2][0, 1] + test[2][0, 0]
+
+        for x, y, dev, loc_tmp in zip(test[0].itertuples(), test[1].itertuples(), timingDevLocal, localTempo):
+            pitch = ix_to_lex.get(x.pitch)
+            if pitch:
+                pitch = pitch[0]
+                start += ((x.beatDiff * moments['beatDiff'][1] + moments['beatDiff'][0]) / (1e-6 + math.exp(loc_tmp))
+                          - (dev * moments['timingDevLocal'][1] + moments['timingDevLocal'][0]))
+                end = start + (x.duration * moments['duration'][1] + moments['duration'][0]) / (1e-6 + math.exp(loc_tmp))
+                note = pretty_midi.Note(100, pitch, start, end)
+                if x.instrument_1:
+                    piano.notes.append(note)
+                else:
+                    violin.notes.append(note)
+    else:
+        raise ValueError("Unknown method")
+
+    return pm