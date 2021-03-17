32d924ddc457230fa5112b586e41b5842be0e727
Fabio Ortega <fabiojose.muneratti@upf.edu>
2021-03-17 12:13:16 +0100

    Changed LR scheduling to steps.

--- train_job.sh
+++ train_job.sh
@@ -18,22 +18,22 @@ export PYTHONFAULTHANDLER=1
 export PATH="$HOME/deep-exp/anaconda3/bin:$PATH"
 source activate envdeep-exp
 
-st=2021-03-15-hp100-128-ie4-12ep.pth
-val=data/m17_val_sequences.data
+st=2021-03-16-hp100-128-ie4.pth
+val=data/mF_val_sequences.data
 
-srun python seq2seq.py data/m17_train_sequences.data \
+srun python seq2seq.py data/mF_train_sequences.data \
 --val-data $val \
 --model-state $st \
 --gen-attr velocity \
---vocab-size 91 \
---lr 1e-4 \
+--vocab-size 92 \
+--lr 1e-5 \
 --hidden-size 128 \
 --dropout 0.1 \
 --batch-size 64 \
 --seq-len 100 \
 --stride 60 \
 --context 20 \
---epochs 4 \
+--epochs 1 \
 --scheduler-step 3 \
 --lr-decay-by 0.1 \
 --workers 4
@@ -46,7 +46,7 @@ srun python seq2seq.py $val \
 --eval \
 --model-state $st \
 --gen-attr velocity \
---vocab-size 91 \
+--vocab-size 92 \
 --hidden-size 128 \
 --dropout 0.1 \
 --batch-size 64 \
